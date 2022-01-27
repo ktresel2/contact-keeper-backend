099b0816b8fbf5b09c12f35b7173a27cc701adff
ktresel2 <ktreseler2@gmail.com>
2022-01-26 21:59:36 -0500

    Add routes

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) =>
+	res.json({ msg: 'Welcome to the contactKeeper API' })
+)
+
+// Define routes
+app.use('/api/users', require('./routes/users'))
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/contacts', require('./routes/contacts'))
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
